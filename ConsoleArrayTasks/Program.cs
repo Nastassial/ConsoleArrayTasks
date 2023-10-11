cc2625a8456f05a8541ce850b6251b32c4763212
Nastassial <nastassialmezhen@gmail.com>
2023-10-11 23:45:47 +0300

    Functions were improved

--- ConsoleArrayTasks/Program.cs
+++ ConsoleArrayTasks/Program.cs
@@ -1,23 +1,131 @@
-﻿Console.WriteLine("--- Задание 1 ---\n");
-Console.WriteLine("Первые 11 членов последовательности Фибоначчи:");
-FibonacciNum(11);
+﻿
+int taskNumAll = 6, taskNum = 1;
+int[] arr;
 
-Console.WriteLine("\n\n\n--- Задание 2 ---\n");
-GetDepositSum();
+while (taskNum <= taskNumAll)
+{
+    Console.WriteLine($"\n\n--- Задание {taskNum} ---\n");
 
-Console.WriteLine("\n\n--- Задание 3 ---\n");
-GetEvenArray();
+    switch (taskNum)
+    {
+        case 1:
+            Console.WriteLine("Первые 11 чисел последовательности Фибоначчи:");
+            ShowFibonacciNumList(11);
 
-Console.WriteLine("\n\n--- Задание 4 ---\n");
-GetArrayWith0();
+            break;
 
-Console.WriteLine("\n\n--- Задание 5 ---\n");
-GetSortNameList();
+        case 2:
+            float depositSum;
+            int monthCnt;
+
+            Console.WriteLine("Введите сумму вклада:");
+            depositSum = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите количество месяцев:");
+            monthCnt = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Сумма вклада с процентами за {monthCnt} месяцев:");
+            Console.WriteLine(GetDepositSum(depositSum, monthCnt));
+
+            break;
+
+        case 3:
+            int arrLength;
+
+            while (true)
+            {
+                Console.WriteLine("Введите размер массива(возможный размер: от 6 до 10 включительно):");
+
+                arrLength = int.Parse(Console.ReadLine());
+
+                if (arrLength > 5 && arrLength < 11) break;
+
+                Console.WriteLine("Некорректное число!");
+            }
+
+            arr = GetRandomArray(arrLength, 1, 101);
+            ShowArray(arr);
+
+            Console.WriteLine("Массив из четных чисел заданного массива:");
+            ShowArray(GetEvenArray(arr));
+
+            break;
+
+        case 4:
+            Console.WriteLine("Массив рандомных чисел от 1 до 100:");
+            arr = GetRandomArray(5, 1, 101);
+            ShowArray(arr);
+
+            Console.WriteLine("Массив, в котором четные элементы = 0:");
+            ShowArray(GetArrayWith0(arr));
+
+            break;
+
+        case 5:
+            string[] arrSort = new string[5] { "Маша", "Максим", "Петя", "Катя", "Аня" };
+
+            Console.WriteLine("Неотсортированный массив имен:");
+            ShowStringArray(arrSort);
+
+            Console.WriteLine("Отсортированный массив имен:");
+            Array.Sort(arrSort);
+            ShowStringArray(arrSort);
+
+            break;
+
+        case 6:
+            Console.WriteLine("Массив рандомных чисел от 1 до 100:");
+            arr = GetRandomArray(15, 1, 101);
+            ShowArray(arr);
+
+            Console.WriteLine("Сортировка пузырьком:");
+
+            BubbleSort(arr);
+            ShowArray(arr);
+
+            break;
+    }
+
+    taskNum++;
+}
 
 Console.WriteLine();
 
 
-static void FibonacciNum(int num)
+static int[] GetRandomArray(int length, int startNum, int endNum)
+{
+    int[] array = new int[length];
+    Random rand = new Random();
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rand.Next(startNum, endNum);
+    }
+
+    return array;
+}
+
+static void ShowArray(int[] array)
+{
+    foreach (int i in array)
+    {
+        Console.Write(i + " ");
+    }
+
+    Console.WriteLine();
+}
+
+static void ShowStringArray(string[] array)
+{
+    foreach (string str in array)
+    {
+        Console.Write(str + " ");
+    }
+
+    Console.WriteLine();
+}
+
+static void ShowFibonacciNumList(int num)
 {
     int res = 0, sum = 1, tmp;
 
@@ -30,116 +138,70 @@ static void FibonacciNum(int num)
     }
 }
 
-static void GetDepositSum()
+static float GetDepositSum(float sum, int monthCnt)
 {
-    float depositSum;
-    int monthCnt;
-
-    Console.WriteLine("Введите сумму вклада и количество месяцев:");
-    depositSum = float.Parse(Console.ReadLine());
-
-    Console.WriteLine("Введите количество месяцев:");
-    monthCnt = int.Parse(Console.ReadLine());
-
     for (int i = 0; i < monthCnt; i++)
     {
-        depositSum += depositSum * 7 / 100;
+        sum += sum * 0.07f;
         //Console.WriteLine(depositSum + " " + (i + 1) + " месяц");
     }
 
-    Console.WriteLine(depositSum);
+    return sum;
 }
 
-static void GetEvenArray()
+static int[] GetEvenArray(int[] initArray)
 {
-    int arrLength, counter = 0;
+    int counter = 0;
 
-    while (true)
+    for (int i = 0; i < initArray.Length; i++)
     {
-        Console.WriteLine("Введите размер массива(возможный размер: от 6 до 10 включительно):");
-
-        arrLength = int.Parse(Console.ReadLine());
-
-        if (arrLength > 5 && arrLength < 11) break;
-            
-        Console.WriteLine("Некорректное число!");
+        if (initArray[i] % 2 == 0) counter++;
     }
 
-    int[] arrRandom = new int[arrLength];
-    Random rand = new Random();
+    int[] evenArray = new int[counter];
 
-    for (int i = 0; i < arrLength; i++)
+    for (int i = 0, j = 0; i < initArray.Length; i++)
     {
-        arrRandom[i] = rand.Next(1, 101);
-
-        Console.Write(arrRandom[i] + " ");
-
-        if (arrRandom[i] % 2 == 0) counter++;
-    }
-
-    Console.WriteLine();
-
-    int[] arrEven = new int[counter];
-
-    for (int i = 0, j = 0; i < arrLength; i++)
-    {
-        if (arrRandom[i] % 2 == 0)
+        if (initArray[i] % 2 == 0)
         {
-            arrEven[j] = arrRandom[i];
-
-            Console.Write(arrEven[j] + " ");
+            evenArray[j] = initArray[i];
             
             j++;
         }
     }
 
-    Console.WriteLine();
+    return evenArray;
 }
 
-static void GetArrayWith0()
+static int[] GetArrayWith0(int[] initArray)
 {
-    int[] arrRandom = new int[5];
-    Random rand = new Random();
+    int[] tmpArray = new int[initArray.Length];
 
-    Console.WriteLine("Массив рандомных чисел от 1 до 100:");
-
-    for (int i = 0; i < arrRandom.Length; i++)
+    for (int i = 0; i < tmpArray.Length; i++)
     {
-        arrRandom[i] = rand.Next(1, 101);
-
-        Console.Write(arrRandom[i] + " ");
+        if (i % 2 != 0)
+            tmpArray[i] = 0;
+        else
+            tmpArray[i] = initArray[i];
     }
 
-    Console.WriteLine("\nМассив рандомных чисел от 1 до 100 с измененым на 0 значением элементов с нечетными индексами:");
-
-    for (int i = 0; i < arrRandom.Length; i++)
-    {
-        if (i % 2 != 0) 
-            arrRandom[i] = 0;
-
-        Console.Write(arrRandom[i] + " ");
-    }
-
-    Console.WriteLine();
+     return tmpArray;
 }
 
-static void GetSortNameList()
+static void BubbleSort(int[] initArray)
 {
-    string[] arrSort = new string[5] { "Маша", "Максим", "Петя", "Катя", "Аня"};
+    int tmp;
 
-    Console.WriteLine("Неотсортированный массив имен:");
-
-    foreach (string s in arrSort)
+    for (int j = 1; j < initArray.Length; j++)
     {
-        Console.Write(s + " ");
-    }
-
-    Console.WriteLine("\nОтсортированный массив имен:");
-
-    Array.Sort(arrSort);
-
-    foreach (string s in arrSort)
-    {
-        Console.Write(s + " ");
+        for (int i = 0; i < initArray.Length - j; i++)
+        {
+            if (initArray[i] > initArray[i + 1])
+            {
+                tmp = initArray[i];
+                initArray[i] = initArray[i + 1];
+                initArray[i + 1] = tmp;
+            }
+        }
     }
 }
